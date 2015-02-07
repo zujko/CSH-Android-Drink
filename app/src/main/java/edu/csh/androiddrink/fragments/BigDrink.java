836cee67a45feed64479e90e9257253fc4f4b8a0
Peter766 <peter7661211@gmail.com>
2015-02-06 20:53:23 -0500

    Show dialog when a list item is clicked.

--- app/src/main/java/edu/csh/androiddrink/fragments/BigDrink.java
+++ app/src/main/java/edu/csh/androiddrink/fragments/BigDrink.java
@@ -1,22 +1,29 @@
 package edu.csh.androiddrink.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import edu.csh.androiddrink.interfaces.MachineDataOnComplete;
 import edu.csh.androiddrink.DrinkAdapter;
-import edu.csh.androiddrink.backgroundtasks.GetMachineItems;
 import edu.csh.androiddrink.ItemInfo;
+import edu.csh.androiddrink.backgroundtasks.GetMachineItems;
+import edu.csh.androiddrink.interfaces.MachineDataOnComplete;
 
 
 public class BigDrink extends ListFragment implements MachineDataOnComplete {
 
+    ArrayList<ItemInfo> itemInfoArrayList;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -27,6 +34,31 @@ public class BigDrink extends ListFragment implements MachineDataOnComplete {
     public ListAdapter getListAdapter() {
         return super.getListAdapter();
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle("Drop a drink?");
+        alert.setMessage("You are about to drop a "+itemInfoArrayList.get(position).getItemName()+
+        "\n\nEnter seconds until drop");
+        final EditText text = new EditText(getActivity());
+        text.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(text);
+        alert.setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).setPositiveButton("Drop!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO: Send POST request to drop drink
+                Toast.makeText(getActivity(),"DROPPED",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
+   }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +75,7 @@ public class BigDrink extends ListFragment implements MachineDataOnComplete {
 
     @Override
     public void onComplete(ArrayList<ItemInfo> items) {
+        itemInfoArrayList = items;
         DrinkAdapter adapter = new DrinkAdapter(getActivity(),items);
         setListAdapter(adapter);
     }
--- app/src/main/java/edu/csh/androiddrink/fragments/LittleDrink.java
+++ app/src/main/java/edu/csh/androiddrink/fragments/LittleDrink.java
@@ -1,11 +1,17 @@
 package edu.csh.androiddrink.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,8 @@ import edu.csh.androiddrink.backgroundtasks.GetMachineItems;
 import edu.csh.androiddrink.ItemInfo;
 
 public class LittleDrink extends ListFragment implements MachineDataOnComplete {
+
+    ArrayList<ItemInfo> itemInfoArrayList;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -32,6 +40,31 @@ public class LittleDrink extends ListFragment implements MachineDataOnComplete {
     }
 
     @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle("Drop a drink?");
+        alert.setMessage("You are about to drop a "+itemInfoArrayList.get(position).getItemName()+
+                "\n\nEnter seconds until drop");
+        final EditText text = new EditText(getActivity());
+        text.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(text);
+        alert.setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).setPositiveButton("Drop!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO: Send POST request to drop drink
+                Toast.makeText(getActivity(), "DROPPED", Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
+    }
+
+    @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
         GetMachineItems sync = new GetMachineItems(this,2);
@@ -41,6 +74,7 @@ public class LittleDrink extends ListFragment implements MachineDataOnComplete {
 
     @Override
     public void onComplete(ArrayList<ItemInfo> items) {
+        itemInfoArrayList = items;
         DrinkAdapter adapter = new DrinkAdapter(getActivity(),items);
         setListAdapter(adapter);
     }

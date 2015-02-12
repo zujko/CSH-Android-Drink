77a0a99f92fa9b972eaf546017efa1e4bc7d8be0
Peter766 <peter7661211@gmail.com>
2015-02-12 00:03:36 -0500

    Add second theme

--- app/src/main/java/edu/csh/androiddrink/MainActivity.java
+++ app/src/main/java/edu/csh/androiddrink/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends ActionBarActivity implements UserDataOnComplet
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
 
         tabs.setViewPager(pager);
-        tabs.setTextColor(Color.parseColor("#FFFFFF"));
         tabs.setIndicatorColor(Color.parseColor("#9D4799"));
         tabs.setIndicatorHeight(7);
     }

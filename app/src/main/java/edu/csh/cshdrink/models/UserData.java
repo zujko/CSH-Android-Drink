c657dbe443fa3043c5150b1cc0cff7713ac0cfcf
zujko <zujko@csh.rit.edu>
2015-10-11 20:46:19 -0400

    Created UserData model

---
+++ app/src/main/java/edu/csh/cshdrink/models/UserData.java
@@ -0,0 +1,19 @@
+package edu.csh.cshdrink.models;
+
+public class UserData {
+    public String status;
+
+    public String message;
+
+    public User data;
+
+    public class User {
+        public String uid;
+
+        public String credits;
+
+        public String admin;
+
+        public String cn;
+    }
+}
